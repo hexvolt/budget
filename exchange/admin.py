4cfd6033be95ba7ef1c5fafea1bddc4617c42d42
hexvolt <hexvolt@gmail.com>
2017-07-12 23:11:31 +0300

    issue_3: exchange rate admin

--- exchange/admin.py
+++ exchange/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from exchange.models import Currency, Conversion, ExchangeRate
+
+
+class ExchangeRateAdmin(admin.ModelAdmin):
+    fields = ('bank', 'date', ('currency_from', 'rate', 'currency_to'))
+    list_display = ('bank', 'date', 'direction', 'rate')
+    list_filter = ('bank',)
+
+
+admin.site.register(ExchangeRate, ExchangeRateAdmin)
+
+admin.site.register((Currency, Conversion))
--- exchange/models.py
+++ exchange/models.py
@@ -1,14 +1,19 @@
+import datetime
+
 from django.db import models
 
 
 class Currency(models.Model):
 
     name = models.CharField(max_length=255)
-    iso_code = models.CharField(max_length=3)
+    iso_code = models.CharField(max_length=3, unique=True)
 
     class Meta:
         db_table = 'currency'
         ordering = ['iso_code']
+
+    def __str__(self):
+        return f'{self.name}'
 
 
 class ExchangeRate(models.Model):
@@ -18,7 +23,7 @@ class ExchangeRate(models.Model):
                              on_delete=models.CASCADE)
 
     rate = models.DecimalField(max_digits=18, decimal_places=9)
-    date = models.DateTimeField()
+    date = models.DateTimeField(default=datetime.datetime.now)
 
     currency_from = models.ForeignKey('exchange.Currency',
                                       related_name='exchange_rates_from',
@@ -31,6 +36,10 @@ class ExchangeRate(models.Model):
     class Meta:
         db_table = 'exchange_rate'
         ordering = ['date']
+
+    @property
+    def direction(self):
+        return f'{self.currency_from}/{self.currency_to}'
 
 
 class Conversion(models.Model):
