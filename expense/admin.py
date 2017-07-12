14c3c17e0e1865c4bc7e2648a1f9d9b4e664cb7c
hexvolt <hexvolt@gmail.com>
2017-07-12 23:37:44 +0300

    issue_3: expense admin

--- expense/admin.py
+++ expense/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from expense.models import Category, Expense
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    fields = (('name', 'order'),)
+    list_display = ('name', 'order')
+
+
+class ExpenseAdmin(admin.ModelAdmin):
+    fields = ('category', ('amount', 'currency'), 'date', 'description')
+    list_display = ('category', 'date', 'amount', 'currency', 'description')
+    list_filter = ('category', 'currency')
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Expense, ExpenseAdmin)
