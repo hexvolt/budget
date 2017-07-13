9d02d44fea1e05fb0a49ed2f4911b308dd95b204
hexvolt <hexvolt@gmail.com>
2017-07-13 23:05:59 +0300

    issue_9: urls split, settings PEP8

--- budget/urls.py
+++ budget/urls.py
@@ -3,6 +3,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^rest-auth/', include('rest_auth.urls')),
-    url(r'^rest-auth/registration/', include('rest_auth.registration.urls'))
+    url(r'^api/', include('budget.urls_api')),
 ]
