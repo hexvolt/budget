37c0e677658b7a75641a22ae38bb80c6c6052b9b
hexvolt <hexvolt@gmail.com>
2017-07-18 10:00:32 +0300

    issue_4: banks create api

--- bank/views.py
+++ bank/views.py
@@ -1,14 +1,17 @@
-from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 
-from bank.models import Bank
 from bank.serializers import BankSerializer
 
 
 class BankViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to manage their banks.
-    """
 
-    queryset = Bank.objects.all()
     serializer_class = BankSerializer
+
+    def get_queryset(self):
+        return self.request.user.banks.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save(user=self.request.user)
--- budget/urls_api.py
+++ budget/urls_api.py
@@ -5,7 +5,7 @@ import bank.views
 
 
 router = routers.DefaultRouter()
-router.register(r'banks', bank.views.BankViewSet)
+router.register(r'banks', bank.views.BankViewSet, base_name='bank')
 
 urlpatterns = [
     url(r'^', include(router.urls)),
