32c828c6e12040df729791a1ff236bc9c162a40b
hexvolt <hexvolt@gmail.com>
2017-07-25 22:04:20 +0300

    issue_6: conversion filters added

--- exchange/views/conversion.py
+++ exchange/views/conversion.py
@@ -1,7 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from django_filters import rest_framework as filters
 
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
 
 from exchange.serializers import ConversionSerializer
 
@@ -9,11 +9,19 @@ from exchange.serializers import ConversionSerializer
 class ConversionViewSet(viewsets.ModelViewSet):
 
     serializer_class = ConversionSerializer
-    filter_backends = (DjangoFilterBackend,)
-    filter_fields = (
-        'currency_from',
-        'currency_to',
-    )
+    filter_backends = (DjangoFilterBackend, SearchFilter)
+    filter_fields = {
+        'date': ['gte', 'lt'],
+        'currency_from': ['exact'],
+        'currency_to': ['exact']
+    }
+    search_fields = ('description',)
 
     def get_queryset(self):
         return self.request.user.conversions.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save(user=self.request.user)
