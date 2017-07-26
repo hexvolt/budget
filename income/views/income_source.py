8b4cbc5721d67bb91cc54631c743c03b73b8d54e
hexvolt <hexvolt@gmail.com>
2017-07-26 22:18:13 +0300

    issue_8: income views

---
+++ income/views/income_source.py
@@ -0,0 +1,20 @@
+from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
+
+from income.serializers import IncomeSourceSerializer
+
+
+class IncomeSourceViewSet(viewsets.ModelViewSet):
+
+    serializer_class = IncomeSourceSerializer
+    filter_backends = (SearchFilter,)
+    search_fields = ('name',)
+
+    def get_queryset(self):
+        return self.request.user.income_sources.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save(user=self.request.user)
