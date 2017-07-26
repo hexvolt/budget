9a460143e01e434e6e03fabaa166bcb6725d2de7
hexvolt <hexvolt@gmail.com>
2017-07-26 10:01:27 +0300

    issue_6: expenses API filters

--- expense/views/expense.py
+++ expense/views/expense.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.filters import SearchFilter
 
@@ -8,14 +9,13 @@ from expense.serializers import ExpenseSerializer
 class ExpenseViewSet(viewsets.ModelViewSet):
 
     serializer_class = ExpenseSerializer
-    filter_backends = (SearchFilter,)
+    filter_backends = (DjangoFilterBackend, SearchFilter,)
+    filter_fields = {
+        'expense_category': ['exact'],
+        'date': ['gte', 'lt'],
+        'currency': ['exact'],
+    }
     search_fields = ('description',)
 
     def get_queryset(self):
         return Expense.objects.filter(expense_category__user=self.request.user)
-
-    # def perform_create(self, serializer):
-    #     serializer.save(user=self.request.user)
-    #
-    # def perform_update(self, serializer):
-    #     serializer.save(user=self.request.user)
