5413a162cec761dcac0fcec4552c2125a595aeca
hexvolt <hexvolt@gmail.com>
2017-08-07 22:35:55 +0300

    issue_12: package.json and webpack config updated

--- budget/settings.py
+++ budget/settings.py
@@ -138,12 +138,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'frontend', 'src'),
 )
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,30 +1,32 @@
-var path = require("path");
-var webpack = require('webpack');
-var BundleTracker = require('webpack-bundle-tracker');
+const path = require('path');
+const webpack = require('webpack');
+const BundleTracker = require('webpack-bundle-tracker');
 
-module.exports = {
+const config = {
   context: __dirname,
 
-  entry: './assets/js/index', // entry point of app
+  entry: './src/js/index.js', // entry point of app
 
   output: {
-      path: path.resolve('./assets/bundles/'),
-      filename: "[name]-[hash].js",
+      path: path.resolve('./bundles/'),
+      filename: "[name]-[hash].js"
   },
-
-  plugins: [
-    new BundleTracker({filename: './webpack-stats.json'}),
-  ],
 
   module: {
     loaders: [
       // transform JSX into JS:
-      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader'},
-    ],
+      { test: /\.jsx?$/, exclude: /node_modules/, use: 'babel-loader'}
+    ]
   },
 
+  plugins: [
+    new BundleTracker({filename: './webpack-stats.json'})
+  ],
+
   resolve: {
-    modulesDirectories: ['node_modules', 'bower_components'],
-    extensions: ['', '.js', '.jsx']
-  },
-}
+    modules: ['node_modules', 'bower_components'],
+    extensions: ['*', '.js', '.jsx']
+  }
+};
+
+module.exports = config;
