92c60296e56ae78f1d70e117ef8afdc3ca5cb574
hexvolt <hexvolt@gmail.com>
2017-08-09 23:46:48 +0300

    issue_12: Dummy landing page/view

--- budget/settings.py
+++ budget/settings.py
@@ -63,8 +63,8 @@ ROOT_URLCONF = 'budget.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': ['./frontend/src/html/'],
+        'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
