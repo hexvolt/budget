81056cf48317428314196e8888c1b077db7533d1
hexvolt <hexvolt@gmail.com>
2017-08-12 18:12:08 +0300

    issue_12: js logic added

--- frontend/bundles/bundle.js
+++ frontend/bundles/bundle.js
@@ -9740,6 +9740,8 @@ module.exports = getHostComponentFromComposite;
 "use strict";
 
 
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _react = __webpack_require__(82);
@@ -9788,7 +9790,12 @@ var Board = function (_React$Component) {
     key: 'handleClick',
     value: function handleClick(i) {
       var squares = this.state.squares.slice();
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+
       squares[i] = this.state.xIsNext ? 'X' : 'O';
+
       this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
     }
   }, {
@@ -9806,7 +9813,14 @@ var Board = function (_React$Component) {
   }, {
     key: 'render',
     value: function render() {
-      var status = 'Next player: X';
+      var winner = calculateWinner(this.state.squares);
+      var status = void 0;
+
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
 
       return _react2.default.createElement(
         'div',
@@ -9876,6 +9890,21 @@ var Game = function (_React$Component2) {
 
   return Game;
 }(_react2.default.Component);
+
+function calculateWinner(squares) {
+  var lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+  for (var i = 0; i < lines.length; i++) {
+    var _lines$i = _slicedToArray(lines[i], 3),
+        a = _lines$i[0],
+        b = _lines$i[1],
+        c = _lines$i[2];
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 
 // ========================================
 
