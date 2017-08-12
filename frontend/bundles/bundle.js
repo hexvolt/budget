178b0adc4906c9b5fba74d9067668d88210b3834
hexvolt <hexvolt@gmail.com>
2017-08-12 11:37:37 +0300

    issue_12: component state introduced

--- frontend/bundles/bundle.js
+++ frontend/bundles/bundle.js
@@ -9766,13 +9766,26 @@ var Square = function (_React$Component) {
   function Square() {
     _classCallCheck(this, Square);
 
-    return _possibleConstructorReturn(this, (Square.__proto__ || Object.getPrototypeOf(Square)).apply(this, arguments));
+    var _this = _possibleConstructorReturn(this, (Square.__proto__ || Object.getPrototypeOf(Square)).call(this));
+
+    _this.state = {
+      value: null
+    };
+    return _this;
   }
 
   _createClass(Square, [{
     key: 'render',
     value: function render() {
-      return _react2.default.createElement('button', { className: 'square' });
+      var _this2 = this;
+
+      return _react2.default.createElement(
+        'button',
+        { className: 'square', onClick: function onClick() {
+            return _this2.setState({ value: 'X' });
+          } },
+        this.state.value
+      );
     }
   }]);
 
@@ -9791,7 +9804,7 @@ var Board = function (_React$Component2) {
   _createClass(Board, [{
     key: 'renderSquare',
     value: function renderSquare(i) {
-      return _react2.default.createElement(Square, null);
+      return _react2.default.createElement(Square, { value: i });
     }
   }, {
     key: 'render',
