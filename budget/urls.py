b8c5d22f09627c094b0f56161596c7628f21c49d
hexvolt <hexvolt@gmail.com>
2017-08-19 10:11:42 +0300

    issue_21: backend routing

--- budget/urls.py
+++ budget/urls.py
@@ -4,9 +4,9 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('budget.urls_api')),
     url(r'^api-auth/', include('rest_framework.urls')),
+    url(r'^', TemplateView.as_view(template_name='index.html')),
 
 ]
