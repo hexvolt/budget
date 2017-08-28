b6a672b8734e36291afac8b19258bbc3efa47b8f
hexvolt <hexvolt@gmail.com>
2017-08-29 00:12:38 +0300

    issue_21: about component

---
+++ frontend/src/components/about/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const About = () => (
+  <div>
+    <h1>About</h1>
+    <ul>
+      <li>email</li>
+      <li>phone</li>
+    </ul>
+  </div>
+);
+
+export default About;
